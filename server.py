9ff7c88eb5854cc82ccadbcc4fdd516ecef59eb5
alangrafu <alvaro@graves.cl>
2012-08-07 09:07:13 -0700

    Put hooks to be developed later

--- server.py
+++ server.py
@@ -1,5 +1,4 @@
-import bottle
-from bottle import response
+from bottle import response, request
 import commands
 import socket
 import re
@@ -12,12 +11,27 @@ appPort=15151
 host = "http://%s:%d/" % (str(socket.gethostname()), appPort)
 hostUri = host+"host"
 
+## Needs to be improved
+def __serialization(g=None):
+  if g is None:
+    return ""
+  return g.serialize(format="n3")
+
+#Gets what type of service this process is running
+def __getServiceType(pid=0, g=None):
+  if pid > 0 or g is None:
+    return
+  ##Add triples to the graph
+  ##For example, that URI for that PID is of rdf:type :SparqlEndpoint
+  return
+
+
 app = bottle.app()
 app.debug=True
 @app.route('/services')
 def sohowServices():
     g = Graph()
-    EX = Namespace("http://example.org/")
+    S = Namespace("http://tw.rpi.edu/srs/")
     RDF = Namespace("http://www.w3.org/1999/02/22-rdf-syntax-ns#")
     XSD = Namespace("http://www.w3.org/2001/XMLSchema#")
     result = commands.getoutput('netstat -tnap|egrep "LISTEN"')
@@ -29,12 +43,13 @@ def sohowServices():
         processPort = ''.join(str(processPortMatch.group(0)).split(":")[1:])
         processPid, processName = str(pairPidProcess.group(0)).split("/")
         serviceUri = host+"pid/"+processPid
-        g.add((URIRef(serviceUri), RDF['type'], EX['Service']))
-        g.add((URIRef(serviceUri), EX['runningPort'], Literal(processPort, datatype=XSD['integer'])))
-        g.add((URIRef(serviceUri), EX['runningHost'], URIRef(hostUri)))
+        g.add((URIRef(serviceUri), RDF['type'], S['Service']))
+        g.add((URIRef(serviceUri), S['runningPort'], Literal(processPort, datatype=XSD['integer'])))
+        g.add((URIRef(serviceUri), S['runningHost'], URIRef(hostUri)))
+        __getServiceType(processPid, g)
 
     response.content_type = 'text/turtle'
-    g.add((URIRef(hostUri), RDF['type'], EX['Host']))
-    return g.serialize(format="n3")
+    g.add((URIRef(hostUri), RDF['type'], S['Host']))
+    return __serialization(g)
 
 bottle.run(app=app,host='0.0.0.0',port=appPort)
